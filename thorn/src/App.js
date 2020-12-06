812eb1e7ce0cb4285cbf998196602186bfdb65fe
scottzockoll <32425272+scottzockoll@users.noreply.github.com>
2020-12-05 19:21:00 -0500

    Ability to add rules to a ruleBook

--- thorn/src/App.js
+++ thorn/src/App.js
@@ -15,24 +15,57 @@ const NewRuleButton = (props) => {
     return (
         <Row>
             <Col>
-                <Button onClick={props.onClick} key={props.index} label={props.index} variant="outline-info" block>Add a rule</Button>
+                <Button onClick={props.onClick} key={props.index} label={props.index} variant="outline-info" block>Add a
+                    rule</Button>
             </Col>
         </Row>
     )
 }
 
+const InputString = (props) => {
+    return (
+        <InputGroup className="mb-3">
+            <FormControl
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+                onKeyPress={event => {
+                    if (event.key === "Enter") {
+                        props.onClick();
+                    }
+                }}
+            />
+            <InputGroup.Append>
+                <Button variant="outline-success" key={"evaluate"} onClick={props.onClick}>Evaluate</Button>
+            </InputGroup.Append>
+        </InputGroup>
+    )
+}
+
 const Rule = (props) => {
+    const [key, setKey] = useState('')
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        props.onChange({[props.index]: [key, value]})
+    })
+
     return (
         <InputGroup className="mb-3">
             <FormControl
                 placeholder={props.index}
                 aria-label="Recipient's username"
                 aria-describedby="basic-addon2"
+                onChange={event => {
+                    setKey(event.target.value)
+                }}
             />
             <FormControl
                 placeholder="Value"
                 aria-label="Recipient's username"
                 aria-describedby="basic-addon2"
+                onChange={event => {
+                    setValue(event.target.value)
+                }}
             />
             <InputGroup.Append>
                 <Button variant="outline-danger" key={props.index} onClick={props.onClick}>Delete</Button>
@@ -43,7 +76,6 @@ const Rule = (props) => {
 
 const Rules = (props) => {
     const [amount, setAmount] = useState(props.amount);
-
     const [list, setList] = useState([...Array(amount).fill().keys()])
 
     const incrementAmount = () => {
@@ -54,28 +86,46 @@ const Rules = (props) => {
     const deleteRule = (index) => {
         setAmount(amount - 1)
         setList(list.filter((key) => key !== index))
+        props.onDeleteRule(index)
     }
 
     return (
         <Card>
             <Card.Body>
+                <InputString onClick={() => console.log('evaluate clicked')}/>
                 <NewRuleButton onClick={incrementAmount}/>
             </Card.Body>
             <Card.Body>
                 {list.map((index) => (
-                    <Rule key={index} index={index} onClick={(e) => deleteRule(index)}/>
+                    <Rule key={index} index={index} onChange={props.onChange} onClick={(e) => deleteRule(index)}/>
                 ))}
             </Card.Body>
         </Card>
     )
 }
 
-const App = () => (
-    <Container className="p-3">
+const Markov = () => {
+    let ruleBook = {}
+
+    const printRuleBook = () => {
+        console.log('Rule book:')
+        console.log(ruleBook)
+    }
+
+    const onDeleteRule = (index) => {
+        delete ruleBook[index]
+    }
+
+    const handleChange = (ruleDefinition) => {
+        ruleBook = {...ruleBook, ...ruleDefinition}
+    }
+
+    return (
         <Container>
+            <button onClick={printRuleBook}>Print ruleBook</button>
             <Row>
                 <Col>
-                    <Rules amount={3}/>
+                    <Rules amount={3} onChange={handleChange} onDeleteRule={onDeleteRule}/>
                 </Col>
                 <Col>
                     <Card>
@@ -84,6 +134,12 @@ const App = () => (
                 </Col>
             </Row>
         </Container>
+    )
+}
+
+const App = () => (
+    <Container className="p-3">
+        <Markov />
     </Container>
 );
 
